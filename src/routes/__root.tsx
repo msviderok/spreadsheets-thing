09eddd7c9e1ff2f5ee7727b36918e5116e5ba7aa
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2026-01-19 09:27:49 +0200

    add meta title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,7 +7,7 @@ export const Route = createRootRoute({
 		meta: [
 			{ charSet: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{ title: "TanStack Start Starter" },
+			{ title: "Додаток 47 – на радість усім" },
 		],
 		links: [{ rel: "stylesheet", href: appCss }],
 	}),
